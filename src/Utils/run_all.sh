2da3bf45acfe02d91ea9b13528ff28a8aadb9f92
Andrea Loretti <56156949+Stintipacchio@users.noreply.github.com>
2024-08-01 23:08:51 +0200

    Added Convalidation 3

--- src/Utils/run_all.sh
+++ src/Utils/run_all.sh
@@ -16,30 +16,38 @@ mkdir -p $RESULTS_DIR
 for CONFIG in $CONFIGS; do
   echo "Running simulation for configuration: $CONFIG"
 
-  # Rimuovi i vecchi file di output se esistono
-  [ -f results.vec ] && rm results.vec
-  [ -f results.sca ] && rm results.sca
-  [ -f results.vci ] && rm results.vci
+  # Numero di run per ogni configurazione
+  RUN_COUNT=$(grep -oP '(?<=^repeat = ).*' $INI_FILE | tr -d '\r')
 
-  # Esegui la simulazione con la configurazione corrente
-  opp_runall -j1 ./TandemQueueSystem_Simulation.exe -u Cmdenv -c $CONFIG -n . -f $INI_FILE
+  # Ciclo attraverso ogni run
+  for ((i=1; i<=RUN_COUNT; i++)); do
+    echo "Running simulation $i for configuration: $CONFIG"
 
-  # Rinomina e sposta i file di output nella directory dei risultati
-  OUTPUT_VEC="${RESULTS_DIR}/${CONFIG}.vec"
-  OUTPUT_SCA="${RESULTS_DIR}/${CONFIG}.sca"
-  OUTPUT_VCI="${RESULTS_DIR}/${CONFIG}.vci"
+    # Rimuovi i vecchi file di output se esistono
+    [ -f results.vec ] && rm results.vec
+    [ -f results.sca ] && rm results.sca
+    [ -f results.vci ] && rm results.vci
 
-  if [ -f results.vec ]; then
-    mv results.vec "$OUTPUT_VEC"
-  fi
+    # Esegui la simulazione con la configurazione corrente
+    opp_runall -j1 ./TandemQueueSystem_Simulation.exe -u Cmdenv -c $CONFIG -r $((i-1)) -n . -f $INI_FILE
 
-  if [ -f results.sca ]; then
-    mv results.sca "$OUTPUT_SCA"
-  fi
+    # Rinomina e sposta i file di output nella directory dei risultati
+    OUTPUT_VEC="${RESULTS_DIR}/${CONFIG}_run${i}.vec"
+    OUTPUT_SCA="${RESULTS_DIR}/${CONFIG}_run${i}.sca"
+    OUTPUT_VCI="${RESULTS_DIR}/${CONFIG}_run${i}.vci"
 
-  if [ -f results.vci ]; then
-    mv results.vci "$OUTPUT_VCI"
-  fi
+    if [ -f results.vec ]; then
+      mv results.vec "$OUTPUT_VEC"
+    fi
+
+    if [ -f results.sca ]; then
+      mv results.sca "$OUTPUT_SCA"
+    fi
+
+    if [ -f results.vci ]; then
+      mv results.vci "$OUTPUT_VCI"
+    fi
+  done
 done
 
 echo "All simulations are completed."
